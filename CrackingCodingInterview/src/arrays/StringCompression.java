fe618bb09fb30e298400313e6e34678d10681bab
saivinay.manapuram@gmail.com <saivinay.manapuram@gmail.com>
2020-01-02 22:39:44 +0530

    arrays version 2

--- CrackingCodingInterview/src/arrays/StringCompression.java
+++ CrackingCodingInterview/src/arrays/StringCompression.java
@@ -3,17 +3,20 @@ package arrays;
 public class StringCompression {
 	
 	public static void main(String[] args) {
-		String s="aabcccccaa";
-		char temp=' ';
+		String s="aabcccccaaa ";
 		int count=0;
 		String unique=" ";
-		for(int i=0;i<s.length();i++)
+		for(int i=0;i<s.length()-1;i++)
 		{
-			if(!unique.contains(s.charAt(i)+""))
+			count++;
+			if(s.charAt(i)!=s.charAt(i+1))
 			{
-				unique=unique+s.charAt(i);
+				unique=unique+s.charAt(i)+count;
+				count=0;
 			}
+			
 		}
+		System.out.println(unique);
 		
 		
 	}
