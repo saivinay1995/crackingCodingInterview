7fd610e49aeafc9e8159187994cbb33f126e0220
saivinay.manapuram@gmail.com <saivinay.manapuram@gmail.com>
2020-01-02 22:47:39 +0530

    v2

--- CrackingCodingInterview/src/arrays/StringRotation.java
+++ CrackingCodingInterview/src/arrays/StringRotation.java
@@ -2,4 +2,14 @@ package arrays;
 
 public class StringRotation {
 
+	public static void main(String[] args) {
+
+
+		String s="waterBottle";
+		for(int i=0;i<s.length();i++)
+		{
+			System.out.println(s.substring(i)+s.substring(0, i));
+			
+		}
+	}
 }
